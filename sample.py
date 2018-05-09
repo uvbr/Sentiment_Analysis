e8e3287e0689278294614067d33ffa9cfdfbb4cc
uvbr <uppuveeru@gmail.com>
2018-05-09 17:26:00 +0530

    keys deleted

--- sample.py
+++ sample.py
@@ -5,10 +5,10 @@ import ast,json
 
 
 #consumer key, consumer secret, access token, access secret.
-ckey="p2UP9MRYjgfpBBYCnqiHsZ2T8"
-csecret="DHajvOpEkVS5td9aBppeDcKcm6auvcKgMhyaVQ0Ks556t1QOi7"
-atoken="2963944907-ilY9wfwV3LYmJmOuaIQGhrpoAK21JVRvNg1Yvap"
-asecret="wFSJPAgw0B04hHhKnk9dSbYdvODxU8b5qZOBoI4beAvZt"
+ckey="xxxxxxxxxxxxxxxxxxxxxxxx"
+csecret="xxxxxxxxxxxxxxxxxxxxxxxxxx"
+atoken="xxxxxxxxxxxx-xxxxxxxxxxxxxxxxx"
+asecret="xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 class listener(StreamListener):
 
